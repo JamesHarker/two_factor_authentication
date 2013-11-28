b10a9e801eb2901009264ef0967666f898381371
James Harker <james@njtechnologies.co.uk>
2013-11-28 17:38:50 +0000

    Allow users to resend code. Move code generation logic into model

--- app/controllers/devise/two_factor_authentication_controller.rb
+++ app/controllers/devise/two_factor_authentication_controller.rb
@@ -16,28 +16,32 @@ class Devise::TwoFactorAuthenticationController < DeviseController
     else
       resource.second_factor_attempts_count += 1
       resource.save
-      set_flash_message :error, :attempt_failed
       if resource.max_login_attempts?
         sign_out(resource)
-        render :template => 'devise/two_factor_authentication/max_login_attempts_reached' and return
+        render 'devise/two_factor_authentication/max_login_attempts_reached'
       else
-        render :show
+        redirect_to user_two_factor_authentication_path, notice: "Invalid code."
       end
     end
   end
 
+  def resend
+    resource.create_two_factor_code
+    redirect_to user_two_factor_authentication_path, notice: "Your authentication code has been sent."
+  end
+
   private
 
-    def authenticate_scope!
-      self.resource = send("current_#{resource_name}")
-    end
+  def authenticate_scope!
+    self.resource = send("current_#{resource_name}")
+  end
 
-    def prepare_and_validate
-      redirect_to :root and return if resource.nil?
-      @limit = resource.class.max_login_attempts
-      if resource.max_login_attempts?
-        sign_out(resource)
-        render :template => 'devise/two_factor_authentication/max_login_attempts_reached' and return
-      end
+  def prepare_and_validate
+    redirect_to :root and return if resource.nil?
+    @limit = resource.class.max_login_attempts
+    if resource.max_login_attempts?
+      sign_out(resource)
+      render 'devise/two_factor_authentication/max_login_attempts_reached'
     end
+  end
 end
